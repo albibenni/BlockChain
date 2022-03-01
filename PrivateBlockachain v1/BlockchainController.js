25c62acf4892d348c70e6cdc0cee8d211dfaf14f
Alberto <alby_b_@hotmail.it>
2022-03-01 19:26:27 +0100

    validatechain

--- PrivateBlockachain v1/BlockchainController.js
+++ PrivateBlockachain v1/BlockchainController.js
@@ -15,6 +15,7 @@ class BlockchainController {
     this.submitStar();
     this.getBlockByHash();
     this.getStarsByOwner();
+    this.validateChain();
   }
 
   // Enpoint to Get a Block by Height (GET Endpoint)
@@ -124,7 +125,7 @@ class BlockchainController {
     });
   }
 
-  validateChainEndPoint() {
+  validateChain() {
     this.app.get("/validateChain", async (req, res) => {
       let errorList = await this.blockchain.validateChain();
       if (errorList.length > 0) {
